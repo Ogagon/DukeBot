420dc036ffa56da6594ab132be204b5c5d2a121b
Goran Trifunović <goran.trifunovic@outlook.com>
2025-12-20 23:40:23 +0100

    Added a new folder for features, and a few folders and classes for future implementations of event schedulers and poll providers.

---
+++ Features/Event Scheduling/ScheduleEventModel.cs
@@ -0,0 +1,18 @@
+﻿using NetCord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DukeBot.Features.Event_Scheduling
+{
+    public class ScheduleEventModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public IEnumerable<EmbedFieldProperties>? Fields { get; set; }
+        public EmbedFooterProperties? Footer { get; set; }
+
+    }
+}
---
+++ Features/Polling/PollModel.cs
@@ -0,0 +1,17 @@
+﻿using NetCord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DukeBot.Features.Polling
+{
+    public class PollModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public IEnumerable<EmbedFieldProperties>? Fields { get; set; }
+        public EmbedFooterProperties? Footer { get; set; }
+    }
+}
